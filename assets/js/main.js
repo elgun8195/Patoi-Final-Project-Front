e1ef24ea82ae40e9561c9970ece54b1c95568fd5
elgun8195 <tu741rmps@code.edu.az>
2022-08-04 23:49:11 +0400

    Solved Slick Slide

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,140 +66,150 @@ $(document).ready(function () {
   $(".go-top").on("click", function () {
     $("html, body").animate({ scrollTop: "0" }, 500);
   });
-  $(".input-counter").each(function() {
-		var spinner = jQuery(this),
-		input = spinner.find('input[type="text"]'),
-		btnUp = spinner.find(".plus-btn"),
-		btnDown = spinner.find(".minus-btn"),
-		min = input.attr("min"),
-		max = input.attr("max");
-		btnUp.on("click", function() {
-			var oldValue = parseFloat(input.val());
-			if (oldValue >= max) {
-				var newVal = oldValue;
-			} else {
-				var newVal = oldValue + 1;
-			}
-			spinner.find("input").val(newVal);
-			spinner.find("input").trigger("change");
-		});
-		btnDown.on("click", function() {
-			var oldValue = parseFloat(input.val());
-			if (oldValue <= min) {
-				var newVal = oldValue;
-			} else {
-				var newVal = oldValue - 1;
-			}
-			spinner.find("input").val(newVal);
-			spinner.find("input").trigger("change");
-		});
-	});
-
+  $(".input-counter").each(function () {
+    var spinner = jQuery(this),
+      input = spinner.find('input[type="text"]'),
+      btnUp = spinner.find(".plus-btn"),
+      btnDown = spinner.find(".minus-btn"),
+      min = input.attr("min"),
+      max = input.attr("max");
+    btnUp.on("click", function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+    btnDown.on("click", function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+  });
 });
-
 
 //price range
 
-
 const rangeInput = document.querySelectorAll(".range-input input"),
-priceInput = document.querySelectorAll(".price-input input"),
-range = document.querySelector(".slider .progress");
+  priceInput = document.querySelectorAll(".price-input input"),
+  range = document.querySelector(".slider .progress");
 let priceGap = 1000;
 
-priceInput.forEach(input =>{
-    input.addEventListener("input", e =>{
-        let minPrice = parseInt(priceInput[0].value),
-        maxPrice = parseInt(priceInput[1].value);
-        console.log(minPrice.value);
-        if((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max){
-            if(e.target.className === "input-min"){
-                rangeInput[0].value = minPrice;
-                range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
-            }else{
-                rangeInput[1].value = maxPrice;
-                range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
-            }
-        }
-    });
+priceInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minPrice = parseInt(priceInput[0].value),
+      maxPrice = parseInt(priceInput[1].value);
+    console.log(minPrice.value);
+    if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
+      if (e.target.className === "input-min") {
+        rangeInput[0].value = minPrice;
+        range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+      } else {
+        rangeInput[1].value = maxPrice;
+        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+      }
+    }
+  });
 });
 
-rangeInput.forEach(input =>{
-    input.addEventListener("input", e =>{
-        let minVal = parseInt(rangeInput[0].value),
-        maxVal = parseInt(rangeInput[1].value);
+rangeInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minVal = parseInt(rangeInput[0].value),
+      maxVal = parseInt(rangeInput[1].value);
 
-        if((maxVal - minVal) < priceGap){
-            if(e.target.className === "range-min"){
-                rangeInput[0].value = maxVal - priceGap
-            }else{
-                rangeInput[1].value = minVal + priceGap;
-            }
-        }else{
-            priceInput[0].value = minVal;
-            priceInput[1].value = maxVal;
-            range.style.left = ((minVal / rangeInput[0].max) * 100) + "%";
-            range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
-        }
-    });
+    if (maxVal - minVal < priceGap) {
+      if (e.target.className === "range-min") {
+        rangeInput[0].value = maxVal - priceGap;
+      } else {
+        rangeInput[1].value = minVal + priceGap;
+      }
+    } else {
+      priceInput[0].value = minVal;
+      priceInput[1].value = maxVal;
+      range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+      range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+    }
+  });
 });
-
 
 //price range
 let inner = document.getElementById("inner");
-      let optioninner = document.getElementById("option-inner");
-      let mexpand2 = document.getElementById("mexpand2");
-      let mexpand3 = document.getElementById("mexpand3");
-      let mexpand4 = document.getElementById("mexpand4");
-      let mmenu = document.getElementById("meanmenu-reveal");
-      let navbarnav = document.getElementById("navbar-nav");
-      let dropdown1 = document.getElementById("dmenu1");
-      let dropdown2 = document.getElementById("dmenu2");
-      let dropdown3 = document.getElementById("dmenu3");
-      let dropdown4 = document.getElementById("dmenu4");      
-        
-       
-        mexpand2.addEventListener("click", () => {
-         
-         if (dropdown2.style.display === "none") {
-           dropdown2.style.display = "block";
-         } else {
-           dropdown2.style.display = "none";
-         }
-       
-   });
-        mexpand3.addEventListener("click", () => {
-        
-         if (dropdown3.style.display === "none") {
-           dropdown3.style.display = "block";
-         } else {
-           dropdown3.style.display = "none";
-         }
-       
-   });
-        mexpand4.addEventListener("click", () => {
-         
-         if (dropdown4.style.display === "none") {
-           dropdown4.style.display = "block";
-         } else {
-           dropdown4.style.display = "none";
-         }
-       
-   });
+let optioninner = document.getElementById("option-inner");
+let mexpand2 = document.getElementById("mexpand2");
+let mexpand3 = document.getElementById("mexpand3");
+let mexpand4 = document.getElementById("mexpand4");
+let mmenu = document.getElementById("meanmenu-reveal");
+let navbarnav = document.getElementById("navbar-nav");
+let dropdown1 = document.getElementById("dmenu1");
+let dropdown2 = document.getElementById("dmenu2");
+let dropdown3 = document.getElementById("dmenu3");
+let dropdown4 = document.getElementById("dmenu4");
 
-        inner.addEventListener("click", () => {
-        optioninner.classList.toggle("active");
-      });
+mexpand2.addEventListener("click", () => {
+  if (dropdown2.style.display === "none") {
+    dropdown2.style.display = "block";
+  } else {
+    dropdown2.style.display = "none";
+  }
+});
+mexpand3.addEventListener("click", () => {
+  if (dropdown3.style.display === "none") {
+    dropdown3.style.display = "block";
+  } else {
+    dropdown3.style.display = "none";
+  }
+});
+mexpand4.addEventListener("click", () => {
+  if (dropdown4.style.display === "none") {
+    dropdown4.style.display = "block";
+  } else {
+    dropdown4.style.display = "none";
+  }
+});
 
-        mmenu.addEventListener("click", () => {
-        mmenu.classList.toggle("meanclose");
-        
-        if (navbarnav.style.display === "none") {
-          navbarnav.style.display = "block";
-        } else {
-          navbarnav.style.display = "none";
-        }
-      });
-      
+inner.addEventListener("click", () => {
+  optioninner.classList.toggle("active");
+});
 
+mmenu.addEventListener("click", () => {
+  mmenu.classList.toggle("meanclose");
 
-      
+  if (navbarnav.style.display === "none") {
+    navbarnav.style.display = "block";
+  } else {
+    navbarnav.style.display = "none";
+  }
+});
+// Product Detaisl Carousel
+const imgs = document.querySelectorAll(".img-select a");
+const imgBtns = [...imgs];
+let imgId = 1;
+
+imgBtns.forEach((imgItem) => {
+  imgItem.addEventListener("click", (event) => {
+    event.preventDefault();
+    imgId = imgItem.dataset.id;
+    slideImage();
+  });
+});
+
+function slideImage() {
+  const displayWidth = document.querySelector(
+    ".img-showcase img:first-child"
+  ).clientWidth;
+
+  document.querySelector(".img-showcase").style.transform = `translateX(${
+    -(imgId - 1) * displayWidth
+  }px)`;
+}
+
+window.addEventListener("resize", slideImage);
+// Product Detaisl Carousel
