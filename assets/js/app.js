66d676cc336c340abf6d2bd1a84461ea2e6ee009
elgun8195 <tu741rmps@code.edu.az>
2022-08-28 16:21:28 +0400

    Updated file

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,14 +6,42 @@ $(document).ready(function () {
       $(".navbar-area").removeClass("is-sticky");
     }
   });
-  
-  $(".others-option .search-icon i").on("click", function(){
-		$(".search-overlay").toggleClass("search-overlay-active");
-	});
-	$(".search-overlay-close").on("click", function(){
-		$(".search-overlay").removeClass("search-overlay-active");
-	});
 
-  
- 
+  $(".others-option .search-icon i").on("click", function () {
+    $(".search-overlay").toggleClass("search-overlay-active");
+  });
+  $(".search-overlay-close").on("click", function () {
+    $(".search-overlay").removeClass("search-overlay-active");
+  });
 });
+
+const prices = document.querySelectorAll("#total-price");
+const cem = document.getElementById("cem");
+const shipping = document.getElementById("shipping");
+const final = document.getElementById("final");
+
+var sum = 0;
+prices.forEach(
+  (x) =>
+    // console.log(parseInt(x.textContent.substring(1)))
+    (sum += parseInt(x.textContent.substring(1)))
+);
+cem.innerHTML = "$" + sum;
+let faiz = (sum * 7) / 100;
+shipping.innerHTML = "$" + faiz;
+final.innerHTML = "$" + (sum + faiz);
+
+
+let qiymet=document.querySelectorAll("#p-price");
+let miqdar=document.querySelectorAll("#pcount");
+
+qiymet.forEach(
+  (x) =>
+    //console.log(parseInt(x.textContent.substring(1)))
+   
+    miqdar.forEach(
+      (x) =>
+        console.log(parseInt(x.textContent))   
+        
+    )
+);
